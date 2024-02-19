97f7c43a3d99f32ccbca5c2277e83303199f26b1
Serah Touati <serah19touati@gmail.com>
2024-02-19 17:58:13 +0100

    Commit 2: 19/02/2024

--- www/controleurs/c_suivrePaiement.php
+++ www/controleurs/c_suivrePaiement.php
@@ -6,4 +6,40 @@
  * and open the template in the editor.
  */
 
-include 'vues/v_suivreFicheFrais.php';
+
+$action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
+$idVisiteur = filter_input(INPUT_POST, 'lstVisiteurs', FILTER_SANITIZE_STRING);
+$leMois = filter_input(INPUT_POST, 'lstMois', FILTER_SANITIZE_STRING);
+
+$lesVisiteurs = $pdo->getNomVisiteurVA();
+
+$visiteurASelectionner = $idVisiteur;
+$moisASelectionner = $leMois;
+$mois = getMois(date('d/m/Y'));
+$lesMois = $pdo-> getLesMoisDisponiblesVA();
+//var_dump($lesMois);
+
+switch ($action) {
+    case 'selectionnerUtilisateur':
+        $lesCles[] = array_keys($lesVisiteurs);
+        $visiteursASelectionner = $lesCles[0];
+        include 'vues/v_visiteurVA.php';
+        break;       
+    
+  case 'valider':
+      echo 'coucou';
+          $leMois = filter_input(INPUT_POST, 'lstMois', FILTER_SANITIZE_STRING);
+    $lesMois = $pdo->getLesMoisDisponibles($idVisiteur);
+    $moisASelectionner = $leMois;
+    $lesFraisHorsForfait = $pdo->getLesFraisHorsForfait($idVisiteur, $leMois);
+    $lesFraisForfait = $pdo->getLesFraisForfait($idVisiteur, $leMois);
+    $lesInfosFicheFrais = $pdo->getLesInfosFicheFrais($idVisiteur, $leMois);
+    $numAnnee = substr($leMois, 0, 4);
+    $numMois = substr($leMois, 4, 2);
+    $libEtat = $lesInfosFicheFrais['libEtat'];
+    $montantValide = $lesInfosFicheFrais['montantValide'];
+    $nbJustificatifs = $lesInfosFicheFrais['nbJustificatifs'];
+    $dateModif = dateAnglaisVersFrancais($lesInfosFicheFrais['dateModif']);
+    include 'vues/v_suivrePaiement.php';
+        break;
+}
